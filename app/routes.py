f9cf8a7a7f7b1c9c6987eb87f5f7bfd0a5de294f
Petter Olofsson <petter@omail.se>
2022-06-04 23:46:37 +0200

    Updated to charger control and added time when to apply charging

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 import os
+from turtle import delay
 from flask import render_template, flash, redirect, url_for, request, send_from_directory
 from app import app
 from app.forms import PowerForm
 from werkzeug.urls import url_parse
 import sys, socket
+from datetime import datetime
 
 
 @app.route('/favicon.ico')
@@ -17,7 +19,20 @@ def index():
 	ip = "192.168.99.143"
 	port = 7090
 	if form.validate_on_submit():
-		limit = form.currlimit.data
+		now = datetime.now()
+		current_hour = int(now.strftime('%H'))
+		current_minute = int(now.strftime('%M'))
+		current_second = int(now.strftime('%S'))
+		start_hour = int(form.chrghour.data)
+		start_minute = int(form.chrgminute.data)
+		if start_hour == 'immidiate':
+			limit = form.currlimit.data + ' 1'	
+		elif start_hour >= current_hour:
+			wait_seconds = (start_hour - current_hour) * 3600 + (start_minute - current_minute) * 60
+			limit = form.currlimit.data + ' ' + str(wait_seconds)
+		else:
+			wait_seconds = (24 + start_hour - current_hour) * 3600 + (start_minute - current_minute) * 60
+			limit = form.currlimit.data + ' ' + str(wait_seconds)
 		limitb = limit.encode(encoding='utf-8')
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.sendto(limitb, (ip, port))
